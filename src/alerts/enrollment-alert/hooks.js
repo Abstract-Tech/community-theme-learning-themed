5c72de25a533df8f4a121efbfd28b44156f5e1c4
Illia Shestakov <illia@ishestakov.com>
2023-05-08 13:55:41 +0300

    Reapply community-theme changes

--- src/alerts/enrollment-alert/hooks.js
+++ src/alerts/enrollment-alert/hooks.js
@@ -1,13 +1,17 @@
 /* eslint-disable import/prefer-default-export */
 import React, {
-  useContext, useMemo,
+  useContext, useState, useCallback, useMemo,
 } from 'react';
+import { sendTrackEvent } from '@edx/frontend-platform/analytics';
 import { AppContext } from '@edx/frontend-platform/react';
 
-import { useAlert } from '../../generic/user-messages';
+import { UserMessagesContext, ALERT_TYPES, useAlert } from '../../generic/user-messages';
 import { useModel } from '../../generic/model-store';
 
-const EnrollmentAlert = React.lazy(() => import('./EnrollmentAlert'));
+import { postCourseEnrollment } from './data/api';
+
+// eslint-disable-next-line no-use-before-define
+const EnrollmentAlert = React.lazy(() => import('./EnrollmentAlert')); // eslint-disable-line no-use-before-define
 
 export function useEnrollmentAlert(courseId) {
   const { authenticatedUser } = useContext(AppContext);
@@ -36,4 +40,29 @@ export function useEnrollmentAlert(courseId) {
   });
 
   return { clientEnrollmentAlert: EnrollmentAlert };
+}
+
+export function useEnrollClickHandler(courseId, orgId, successText) {
+  const [loading, setLoading] = useState(false);
+  const { addFlash } = useContext(UserMessagesContext);
+  const enrollClickHandler = useCallback(() => {
+    setLoading(true);
+    postCourseEnrollment(courseId).then(() => {
+      addFlash({
+        dismissible: true,
+        flash: true,
+        text: successText,
+        type: ALERT_TYPES.SUCCESS,
+        topic: 'course',
+      });
+      setLoading(false);
+      sendTrackEvent('edx.bi.user.course-home.enrollment', {
+        org_key: orgId,
+        courserun_key: courseId,
+      });
+      global.location.reload();
+    });
+  }, [courseId]);
+
+  return { enrollClickHandler, loading };
 }
